09f776e9e1b779a6969a5ffabd058de5aa4dc6c0
mariaflorenciaferreira <florferreirag@gmail.com>
2022-05-19 23:22:47 -0300

    remove style "playing" fixed

--- scripts.js
+++ scripts.js
@@ -10,24 +10,37 @@ const keymap = {
     l: "l"  
 }
 
+let prevkeyElement, prevaudioElement;
+
 function play(key) {
-    var keyElement = document.querySelector(`.key[id="${key}"]`); 
-    var audioElement = document.querySelector(`audio[data-key="${key}"]`);
-    
-    keyElement.classList.add('playing');
+    if (prevkeyElement !== undefined) {
+        prevkeyElement.classList.remove("playing");
+        prevaudioElement.pause();
+
+       
+    }
+
+    const keyElement = document.querySelector(`.key[id="${key}"]`);
+    const audioElement = document.querySelector(`audio[data-key="${key}"]`);
+    prevkeyElement = keyElement;
+    prevaudioElement = audioElement;
+
+    keyElement.classList.add("playing");
     audioElement.currentTime = 0;
     audioElement.play();
+
+    setTimeout(()=>{
+        prevkeyElement.classList.remove("playing")
+    },250);
 }
 
-
-window.addEventListener('keydown',(e)=>{
-    var key = keymap[e.key];
+window.addEventListener("keydown", (e) => {
+    const key = keymap[e.key];
     if (key) {
         play(key);
     }
-})  
+});
 
-
-function clickHandler (key){
+function clickHandler(key) {
     play(key);
 }
